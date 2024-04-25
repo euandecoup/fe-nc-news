82087e94bf87d6149881c4afc8b86431b041b080
euandecoup <151732440+euandecoup@users.noreply.github.com>
2024-04-25 16:08:19 +0100

    styled navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,9 +10,9 @@ function Header () {
         <h1>NC News</h1>
         <h2>Greetings {loggedInUser.username}!</h2>
         <nav className='main-nav'>
-            <Link className="link" key={'Home'} to={`/`}>Home</Link>
-            <Link className="link" key={'Articles'} to={'/articles'}>Articles</Link>
-            <Link className="link" key={'Topics'} to={'/topics'}>Topics</Link>
+            <Link className="nav-item" key={'Home'} to={`/`}>Home</Link>
+            <Link className="nav-item" key={'Articles'} to={'/articles'}>Articles</Link>
+            <Link className="nav-item" key={'Topics'} to={'/topics'}>Topics</Link>
             <a>Users</a>
         </nav>
     </div>
